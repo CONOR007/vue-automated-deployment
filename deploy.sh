e74e5cf3a8daa560886645fda35d4d7659e043c9
xkwg <xkwg@163.com>
2020-11-17 16:16:18 +0800

    自动化部署

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ cnpm install
 npm run build
 
 # 删除 ngnix 指向的文件夹下的文件
-rm -rf /root/vue/*
+rm -rf /home/www/*
 
 # 将打包好的文件复制过去
-mv /tmp/vue-automated-deployment/dist/* /root/vue
+mv /tmp/vue-automated-deployment/dist/* /home/www
 
 # 删除 clone 的代码
 rm -rf /tmp/vue-automated-deployment
